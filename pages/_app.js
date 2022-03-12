768e4d5f11a298e2be806f30f8bcb449b3f11dc4
mrayandika15 <mrayandika15@gmail.com>
2022-03-12 13:58:12 +0700

    feat(fix-chakra-provider)

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 
-import { ChakraProvider } from "@chakra-ui/provider";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
